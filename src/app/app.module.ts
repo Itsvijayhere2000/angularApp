554ec16c0ebbceea4db666a3aec65c1513026e65
vijayr2000 <vijaykrdsm@gmail.com>
2022-09-16 17:43:59 +0530

    unbind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainareaComponent } from './mainarea/mainarea.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LinesComponent } from './lines/lines.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainareaComponent,
+    SidebarComponent,
+    FooterComponent,
+    LinesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+      title:any="enter your name"
+      isdisabled:boolean=false;
+  constructor() { }
+   
+  ngOnInit(): void {
+  }
+myButton(){
+  console.log("clicked ")
+  this.isdisabled=true;
+
+}
+onclick(event:any){
+this.title=event.target.value
+}
+clicking(){
+  this.isdisabled=false;
+}
+}
